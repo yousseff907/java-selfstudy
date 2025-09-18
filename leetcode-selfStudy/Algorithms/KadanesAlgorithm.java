fe93c62729bf881cbac17b8eb9058b6c398f27f8
Youssef <youssef.itani.11.11@gmail.com>
2025-09-18 23:56:07 +0300

    Organized files/folders

--- leetcode-selfStudy/Algorithms/KadanesAlgorithm.java
+++ leetcode-selfStudy/Algorithms/KadanesAlgorithm.java
@@ -4,7 +4,7 @@ Input: [-2,1,-3,4,-1,2,1,-5,4]
 Output: 6 (subarray [4,-1,2,1] has sum 6)
 */
 
-public class maxSum
+public class KadanesAlgorithm
 {
     public static int maxSubArray(int[] nums)
 	{
